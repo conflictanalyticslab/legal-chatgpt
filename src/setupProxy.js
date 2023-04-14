e982a353dae446b9295773ccb98bc9015911d226
Olivia-Chen-Xu <oliviachenxu@gmail.com>
2023-04-14 10:52:46 -0400

    syntax edit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const createProxyMiddleware = require("http-proxy-middleware");
 
-module.exports = function (app) {
+module.exports = function(app) {
     app.use(
         createProxyMiddleware("/scholarsportal", {
             target: "https://journals.scholarsportal.info", // API endpoint 1
