78d731f2168c6e2fd561702cf67cff42bae62e86
Joyce Luo <chufeiluo@gmail.com>
2023-05-12 09:44:16 -0400

    changing things for azure
    pain

--- src/App.js
+++ src/App.js
@@ -11,18 +11,39 @@ WebFont.load({google: {families: ["Roboto:300,400,500"]}});
 import {
     Routes,
     Route,
+    useLocation,
+    redirect,
   } from "react-router-dom";
 
 import LandingPage from './components/pages/LandingPage';
-import ChatPage from './components/pages/ChatPage';
+// import ChatPage from './components/pages/ChatPage';
 
+function NoMatch() {
+    let location = useLocation();
+  
+    return (
+      <div>
+        <h3>
+          No match for <code>{location.pathname}</code>
+        </h3>
+      </div>
+    );
+  }
 
+const ChatRoute = () => {
+    window.location.replace('https://chat.openjustice.ai');
+    return null; };
+  
 function App() {
     return (
               
         <Routes> {/* The Switch decides which component to show based on the current URL.*/}
             <Route path='/' element={<LandingPage />} />
-            <Route path='/chat' element={<ChatPage />} />
+
+            <Route path='/chat' element={<ChatRoute />} />
+            
+            <Route component={NoMatch} />
+            <Route onEnter={() => window.location.reload()} />
         </Routes>
             );
     }
