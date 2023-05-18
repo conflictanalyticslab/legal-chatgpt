8f3a3942561597276f3029a4faf677d936e63c93
chufeiluo <33498986+chufeiluo@users.noreply.github.com>
2023-05-18 18:36:54 -0400

    Updating public resources
    changing title in index.html, adding logo as favicon, updating text on landing page

--- landing/src/components/Hero.js
+++ landing/src/components/Hero.js
@@ -3,8 +3,6 @@ import { Grid, Typography, Button, Box } from '@mui/material';
 import useStyles from '../styles/styles';
 import mid_logo from '../images/OpenJustice_Middle.svg';
 
-import left_tri from '../images/leftTriangle.svg';
-import right_tri from '../images/rightTriangle.svg';
 import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
@@ -16,34 +14,32 @@ const Hero = () => {
 }
 
   return (
+
     <div className={classes.appBackground}>
-      {/* <Grid item xs={12} md={7} margin = '0px' >
-        <img src={left_tri} alt="left_tri" className={classes.medImage} />
-      </Grid>    
-      <Grid item xs={12} md={7} margin = '0px'>
-        <img src={right_tri} alt="right_tri" className={classes.medImage} />
-      </Grid> */}
-    <Box className={classes.heroBox} textAlign="center">
-      <Grid container spacing={7} className={classes.gridContainer} >
       
-      <Grid item xs={12} md={7} margin = '0px'>
+    <Box className={classes.heroBox} textAlign="center">
+      <Grid container spacing={7} className={classes.gridContainer} 
+  direction="column"
+  justifyContent="center"
+  alignItems="center"
+  height='100%'
+>
+      
+      <Grid item xs={12} md={7} width='80%'>
           <img src={mid_logo} alt="logo" className={classes.medImage} />
         </Grid>
-        <Grid item xs={12} md={7}>
+        <Grid item xs={12} md={7} zeroMinWidth width='40%'>
           <Typography variant="h4" fontWeight={700} className={classes.title} textAlign="center">
-            Experience ChatGPT
+          Generative AI for Legal Education and Research
           </Typography>
-          <Typography variant="h4" fontWeight={700} className={classes.title} textAlign="center">
-            Designed for Law
+          <Typography variant="h6" className={classes.subtitle2} sx = {{ marginTop: '30px', fontSize: '18px', }}  >
+          OpenJustice is an open-access AI technology designed to efficiently address legal queries and provide comprehensive answers by drawing upon diverse legal sources and narratives.
           </Typography>
-          <Typography variant="h6" className={classes.subtitle2} sx = {{ marginTop: '60px' }}  >
-              Our advanced AI algorithms are designed to streamline and optimize 
+            <Typography variant="h6" className={classes.subtitle} sx = {{ marginTop: '20px', fontSize: '18px', }} >
+This is a research prototype designed to help law students and lawyers to improve their legal skills. 
             </Typography>
-            <Typography variant="h6" className={classes.subtitle}>
-            legal processes, allowing you to focus on what really matters: your case.
-            </Typography>
-            <Typography variant="h6" className={classes.subtitle}>
-              Specifically trained on actual calculations based on Canadian case law.
+            <Typography variant="h6" className={classes.subtitle} sx = {{ marginTop: '15px',fontSize: '18px', }} >
+OpenJustice is continuously learning and evolving. Please let us know if the answers it produces are not what you expect. 
             </Typography>
             
             <Box className={classes.Box} sx={{ display: 'flex', justifyContent: 'center'}}>
@@ -58,11 +54,13 @@ const Hero = () => {
           <Button
             variant=""
             className={classes.formHeading}
-            sx={{ width: '160px', fontSize: '16px', marginTop: '30px', color: 'black',  textTransform: 'none',  textDecoration: 'underline', fontWeight: 'bold' }}
+            sx={{ width: '160px', fontSize: '16px', marginTop: '30px', color: 'black',  textTransform: 'none',  textDecoration: 'underline', fontWeight: 'bold'}}
           >  <Typography fontWeight= 'bold'>Learn More</Typography>
   </Button>
 </Box>
 
+        </Grid>
+        <Grid item xs={12} md={7} margin = '0px'width='90%'>
         </Grid>
       </Grid>
     </Box>
--- landing/src/components/pages/LandingPage.js
+++ landing/src/components/pages/LandingPage.js
@@ -4,6 +4,7 @@ import Hero from "../Hero"
 import Header from "../Header"
 import Footer from "../Footer"
 import WebFont from "webfontloader";
+
 WebFont.load({google: {families: ["Roboto:300,400,500"]}});
 // import '@fontsource/roboto/300.css';
 // import '@fontsource/roboto/400.css';
@@ -11,7 +12,6 @@ WebFont.load({google: {families: ["Roboto:300,400,500"]}});
 // import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider} from '@mui/material/styles';
-
 
 const theme = createTheme({
     typography: {
@@ -22,13 +22,14 @@ const theme = createTheme({
     },});
 
 function LandingPage() {
+  
     return (
       <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Header />
         <Hero />
-        <Footer />
+        <Footer />    
         </ThemeProvider>
       </>
   
