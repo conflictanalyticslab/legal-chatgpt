17d8dadd925e405ba72e8f960649d8ac33335a8e
Rohan Bhambhoria <rohan.vbh@gmail.com>
2023-08-31 12:54:12 -0400

    Update Login.js

--- src/components/landing/Login.js
+++ src/components/landing/Login.js
@@ -50,7 +50,7 @@ function Login () {
   const emailSchema = Joi.object({
     email: Joi.string()
       .email({tlds: false})
-      .pattern(/^[a-zA-Z0-9._%+-]+@(queensu\.ca|uiowa\.edu|port\.ac\.uk|law\.ucla\.edu|law\.upenn\.edu|lakeheadu\.ca)$/)
+      .pattern(/^[a-zA-Z0-9._%+-]+@(queensu\.ca|uiowa\.edu|port\.ac\.uk|law\.ucla\.edu|law\.upenn\.edu|lakeheadu\.ca|mcgill\.ca|ualberta\.ca|umanitoba\.ca|utoronto\.ca|law\.fsu\.edu|law\.leidenuniv\.nl|library\.ledidenuniv\.nl|univ\-catholille\.fr|law\.harvard\.edu|fas\.harvard\.edu|law\.northwestern\.edu|kellogg\.northwestern\.edu)$/)
       .messages({
         'string.pattern.base': 'Email must be from an approved domain.',
       }),
