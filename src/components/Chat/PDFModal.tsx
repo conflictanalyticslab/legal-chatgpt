99e32d89d14da5b1a6f7d58ceb6acb57a92b9c9e
YHuang <yhuang.huang@mail.utoronto.ca>
2024-01-12 00:30:21 -0500

    modified UI for pdf interaction, implemented backend functionalities to update the text being changed

--- src/components/Chat/PDFModal.tsx
+++ src/components/Chat/PDFModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Modal from '@mui/material/Modal';
 import Image from "next/image";
 import DocsImg from "../../images/doc_img.jpeg";
@@ -10,19 +10,22 @@ import {
     CardContent,
     Grid,
     IconButton,
+    TextField,
     Tooltip,
-    Typography,
+    Typography
 } from "@mui/material";
 import {
     UserDocument,
     getDocumentsOwnedByUser,
 } from "@/util/requests/getDocumentsOwnedByUser";
 // import { useIncludedDocuments } from "@/hooks/useIncludedDocuments"; // passing props instead
+import { editDocument } from "@/util/api/editDocument";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import CheckBoxOutlineIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import { stringify } from "querystring";
+import { set } from "firebase/database";
 
 type Props = {
     documents: UserDocument[];
@@ -52,6 +55,10 @@ export default function PDFModal({ documents, deleteDocument, currentInput, setC
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [editing, setEditing] = useState("");
+  const textFieldRef = useRef<HTMLInputElement>(null);
+  const [inputValue, setInputValue] = useState("");
+  const cardContentRef = useRef<HTMLDivElement>(null);
   
 
 //   useEffect(() => {
@@ -95,9 +102,80 @@ export default function PDFModal({ documents, deleteDocument, currentInput, setC
       <Modal open={open} onClose={handleClose}>
         <Box sx={style}>
         <Typography style={{ justifyContent: "center", marginBottom: "3rem" }} variant="h6" component="h2">Uploaded PDFs</Typography>
+        {editing == "" ? (
         <Grid container spacing={4} columns={4}>
             {documents.map((document) => (
             <Grid item key={document.uid} style={{ width: "25%" }}>
+                <Card>
+                <CardContent>{document.name}</CardContent>
+                <CardContent >{document.text.length > 180 ? (document.text.substring(0, 177) + "...") : document.text}</CardContent>
+                <CardActions>
+                    <Tooltip title="Include in conversation">
+                    <IconButton
+                        key="include-document"
+                        onClick={() => {
+                        if (includedDocuments.includes(document.uid)) {
+                            setCurrentInput(currentInput.replace(document.text, ""))
+                            setIncludedDocuments(
+                            includedDocuments.filter(
+                                (docUid: string) => docUid != document.uid
+                            )
+                            );
+                        } else {
+                            setCurrentInput(currentInput + " " + document.text);
+                            setIncludedDocuments([
+                            ...includedDocuments,
+                            document.uid,
+                            ]);
+                        }
+                        }}
+                        style={{ color: "#006400" }}
+                    >
+                        {includedDocuments.includes(document.uid) ? (
+                        <CheckBoxIcon />
+                        ) : (
+                        <CheckBoxOutlineIcon />
+                        )}
+                    </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Edit document text">
+                    <IconButton
+                        key="edit-document"
+                        onClick={() => {
+                          setEditing(document.uid);
+                          setInputValue(document.text);
+                          if (textFieldRef.current) {
+                            textFieldRef.current.focus();
+                          }
+                        }}
+                        style={{ color: "#000" }}
+                    >
+                        <EditIcon />
+                    </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Delete document">
+                    <IconButton
+                        key="delete-document"
+                        onClick={() => {
+                        console.log("Delete" + document.name);
+                        deleteDocument(document.uid);
+                        }}
+                        style={{ marginRight: "8px", color: "#f33" }}
+                    >
+                        <DeleteIcon />
+                    </IconButton>
+                    </Tooltip>
+                </CardActions>
+                </Card>
+            </Grid>
+            ))}
+        </Grid>
+        ) : (
+          <Grid container spacing={4}>
+    <Grid item xs={3}>
+        <Grid container spacing={4} alignItems="flex-start">
+            {documents.map((document) => (
+            <Grid item key={document.uid} xs={12}>
                 <Card>
                 <CardContent>{document.name}</CardContent>
                 <CardContent>{document.text.length > 180 ? (document.text.substring(0, 177) + "...") : document.text}</CardContent>
@@ -134,7 +212,11 @@ export default function PDFModal({ documents, deleteDocument, currentInput, setC
                     <IconButton
                         key="edit-document"
                         onClick={() => {
-                        console.log("Edit");
+                        setEditing(document.uid);
+                        setInputValue(document.text);
+                        if (textFieldRef.current) {
+                          textFieldRef.current.focus();
+                        }
                         }}
                         style={{ color: "#000" }}
                     >
@@ -158,6 +240,45 @@ export default function PDFModal({ documents, deleteDocument, currentInput, setC
             </Grid>
             ))}
         </Grid>
+    </Grid>
+    <Grid item xs={9}>
+        <TextField 
+            fullWidth 
+            multiline 
+            variant="outlined" 
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            inputRef={textFieldRef}
+        />
+        <Button
+          variant="contained"
+          sx={{
+            width: "160px",
+            fontSize: "16px",
+            marginTop: "30px",
+            color: "white",
+            textTransform: "none",
+            textDecoration: "none",
+            fontWeight: "bold"
+          }}
+          onClick={() => {
+            if (textFieldRef.current && textFieldRef.current.value) {
+              try {
+                editDocument(editing, textFieldRef.current?.value)
+                documents.filter((doc) => doc.uid == editing)[0].text = textFieldRef.current?.value;
+                setEditing("");
+              } catch (e) {
+                console.error("Error editing document: " + e);
+              }
+            }
+
+          }}
+        >
+          Done
+      </Button>
+    </Grid>
+</Grid>
+        )}
           </Box>
       </Modal>
     </div>
