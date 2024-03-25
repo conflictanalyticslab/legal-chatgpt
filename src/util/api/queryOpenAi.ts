07394367d2422256535548787fd5d79b27117f15
YHuang <yhuang.huang@mail.utoronto.ca>
2024-03-25 12:40:48 -0400

    editted prompts_allowed for allowed_list users, also changed base model to be gpt-3.5-turbo-0125

--- src/util/api/queryOpenAi.ts
+++ src/util/api/queryOpenAi.ts
@@ -1,4 +1,6 @@
-export const queryOpenAi = async (data: object) => {
+export const queryOpenAi = async (data: any) => {
+  // specifying gpt model here
+  data["model"] = "gpt-3.5-turbo-0125";
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
