8320a0441c0b21c6e205ffbe67163416aad58319
Khoa Nguyen <dangkhoa81122@gmail.com>
2024-10-30 13:37:15 -0400

    feat(): option to remove df from being used

--- src/app/features/chat/components/conversation-query/conversation-query.tsx
+++ src/app/features/chat/components/conversation-query/conversation-query.tsx
@@ -3,13 +3,14 @@ import { useGlobalContext } from "@/app/store/global-context";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import { SquareMinus } from "lucide-react";
 
 import UploadDocument from "../upload-document/upload-document";
 import { FlowModal } from "../dialog-flows/flow-graph";
 import useFetchQuery from "@/app/features/chat/hooks/use-fetch-query";
 
 export function ConversationQuery() {
-  const { loading, userQuery, setUserQuery, num, stopQuery, dialogFlowName } = useGlobalContext();
+  const { loading, userQuery, setUserQuery, num, stopQuery, dialogFlowName, setDialogFlowName, setDialogFlow } = useGlobalContext();
 
   const { fetchQuery } = useFetchQuery();
   /**
@@ -65,11 +66,26 @@ export function ConversationQuery() {
             ? "No more prompts allowed. Please enter your final feedback."
             : `Prompts left: ${num}`}
         </label>
-        <label className="text-[grey] text-sm absolute bottom-[-20px] right-[0px] italic ">
+        <label className="text-[grey] text-sm absolute bottom-[-20px] right-[30px] italic ">
           {dialogFlowName === ""
             ? "Dialog Flow not in use"
             : "Dialog Flow in use: " + dialogFlowName}
         </label>
+        
+        {dialogFlowName !== "" && (
+          <Button 
+            variant={"ghost"}
+            className="bg-transparent absolute bottom-[-30px] right-[-20px] italic"
+            disabled={dialogFlowName === ""}
+            onClick={() => {
+              setDialogFlow("")
+              setDialogFlowName("")
+            }}
+          >
+            <SquareMinus />
+          </Button>
+        )}
+        
       </div>
     </form>
   );
