95e9a6caff887b3992681e1654fb772408131189
Kevin Wang <kayhne999@gmail.com>
2025-02-16 16:05:11 -0500

    pivoting

--- lawyer-off/app.py
+++ lawyer-off/app.py
@@ -27,7 +27,7 @@ class JudgeMessage:
 st.set_page_config(page_title="Lawyer-Off!", page_icon="⚖️", layout="wide")
 
 st.title("Ace Attorney")
-st.markdown("""Not actual certified legal advice""")
+st.markdown("""*not actual certified legal advice""")
 
 num_args = 1 # default number of arguments both sides get to take
 
@@ -44,28 +44,19 @@ col1, col2 = st.columns([0.5,1])
 with col1:
     # Selection component
     option = st.selectbox("",
-        ("Employment", "Landlord & Tenant", "Immigration")
+        ("", "Employment", "Landlord & Tenant", "Immigration")
     )
     print("User chose area:", option)
 
 with col2:
     # Focus on employment law, Landlord & Tenant law, or Immigration Law
-    match option:
-        case "Employment":
-            with st.expander("Case Info", expanded=True):
-                # Indexing matters a lot in markdown.
-                popup_string = f"""### {test_trial["case"]}
-
-{test_trial["description"]}   
-                            
-### EVIDENCE:
-"""
-                for category, description in test_trial['evidence']:
-                    popup_string += f"- {category} evidence: {description}\n"
-                st.markdown(popup_string)
+    if option:
+        with st.expander("Case Info", expanded=True):
+            # Indexing matters a lot in markdown.
+            st.markdown("Case info goes here")
 
 if option:
-    lawyer, judge, player = Lawyer(defending=True), Judge(), PlayerGenerator(evidence=test_trial["evidence"])
+    lawyer = Lawyer(defending=True)
     
     # Custom CSS for the faded separator
     st.markdown("""
@@ -99,177 +90,57 @@ if option:
         """,
         unsafe_allow_html=True,
     )
-
-    st.markdown(f'<div class="faded-separator">--- Opening Statements ---</div>', unsafe_allow_html=True)
-    st.markdown(f'<div class="faded-separator"> Hint: Prosecution Goes First! </div>', unsafe_allow_html=True)
-
     # Create two columns for the chat windows
     col1, middle, col2 = st.columns([1, 1, 1])
     
     # session state
     if "human_history" not in st.session_state:
-        st.session_state.human_history = [HumanMessage(content="Click on one of the options on the right to get started!")]
+        st.session_state.human_history = []
     if "cpu_history" not in st.session_state:
-        st.session_state.cpu_history = [AIMessage(content="Beep Boop. Ready to Law Out.")]
+        st.session_state.cpu_history = [AIMessage(content="Beep Boop. Ready for action.")]
 
-    # First Chat Window (Left)
-    with col1:
-        st.markdown("### HUMAN")
-        for message in st.session_state.human_history[:1]:    
+    for i in range(len(st.session_state.human_history)):
+        hm, cm = st.session_state.human_history[i], st.session_state.cpu_history[i]
+        with col1:
             with st.chat_message("Human"):
-                st.write(message.content)
+                st.write(hm.content)
                 
-    if "selected_option" not in st.session_state:
-        st.session_state.selected_option = ""
-
-    with middle:
-        options = [
-            "Appeal to Emotion.",
-            "Use Facts and Logic.",
-            "Get Confrontational.",
-            "Tell a Story."
-        ]
-        for choice in options:
-            # Assume users will only pick one
-            if st.button(label=choice, key=choice):
-                print(st.session_state.selected_option)
-                st.session_state.selected_option = choice
-
-                yap = player.respond(strategy=st.session_state.selected_option, typ="opener")
-                print(yap)
-                st.session_state.human_history[0] = HumanMessage(content=yap)
-                
-                response = lawyer.respond(typ="opener")
-                st.session_state.cpu_history[0]=(AIMessage(content=response))
-                
-                if len(st.session_state.human_history) >= 1:
-                    st.session_state.cur_section = 1
-                elif len(st.session_state.human_history) >= 1 + num_args:
-                    st.session_state.cur_section = 2
-                elif len(st.session_state.human_history) >= 1 + num_args + 1:
-                    st.session_state.cur_section = 3 # Judgement & Feedback
-                
-                print(len(st.session_state.human_history), st.session_state.cur_section)
-                st.rerun()
-                
-                
-                
-                
-
-    # Second Chat Window (Right)
-    
-
-    with col2:
-        # Selection Window on Top of the Second Chat Box
-        st.markdown("### CPU")
-        
-        for message in st.session_state.cpu_history[:1]:    
+        with col2:
             with st.chat_message("AI"):
-                st.write(message.content)
+                st.write(cm.content)
+
+    with middle:                
+        st.markdown(f'<div class="faded-separator">--- How to use Ace Attorney: ---</div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="faded-separator"> Try your best </div>', unsafe_allow_html=True)
+
+        
+        user_query = st.chat_input(message)
+        if user_query is not None and user_query != "":
+            st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+        with st.chat_message("Human"):
+            st.markdown(user_query)
+
+        with st.chat_message("AI"):
+            response = lawyer.respond(typ="opener", response=user_query)
+            st.write(response)
+
+        st.session_state.chat_history.append(AIMessage(content=response) if cur_section < 2 else JudgeMessage(content=response))
+        print(len(st.session_state.chat_history), cur_section)
+        
+        if len(st.session_state.chat_history) == 2 + 2*num_args + 2:
+            # Now swap to judge.
+            print("Now judging the court.")
+            response = judge.judge_conversation(convo=st.session_state.chat_history)
+            st.session_state.chat_history.append(JudgeMessage(content=response))
+        
+            # TODO: Also determine the winner
+        
+        st.rerun()
+
                 
-    if st.session_state.cur_section > 0: 
-        st.markdown(f'<div class="faded-separator">--- Presentation of Evidence and Testimony ---</div>', unsafe_allow_html=True)
-        st.markdown(f'<div class="faded-separator"> You each will have {num_args} chances to state your points. </div>', unsafe_allow_html=True)
-
-
-        if len(st.session_state.human_history[1:2+num_args]) < 2:
-            col1, middle, col2 = st.columns([1, 2, 1])
-            # session state
-            with col1:
-                st.markdown("### HUMAN")
-                with st.chat_message("Human"):
-                        st.write("Choose an argument strategy.")
-            
-            with middle:
-                options = [
-                    "Appeal to Emotion.",
-                    "Use Facts and Logic.",
-                    "Get Confrontational.",
-                    "Tell a Story."
-                ]
-                for choice in options:
-                    # Assume users will only pick one
-                    if st.button(label=choice, key=choice + "1"):
-                        print(st.session_state.selected_option)
-                        st.session_state.selected_option = choice
-
-                        yap = player.respond(strategy=st.session_state.selected_option, typ="response")
-                        print(yap)
-                        st.session_state.human_history.append(HumanMessage(content=yap))
-                        
-                        response = lawyer.respond(typ="response")
-                        st.session_state.cpu_history.append(AIMessage(content=response))
-                        
-                        if len(st.session_state.human_history) >= 1:
-                            st.session_state.cur_section = 1
-                        elif len(st.session_state.human_history) >= 1 + num_args:
-                            st.session_state.cur_section = 2
-                        elif len(st.session_state.human_history) >= 1 + num_args + 1:
-                            st.session_state.cur_section = 3 # Judgement & Feedback
-                        
-                        print(len(st.session_state.human_history), len(st.session_state.cpu_history), st.session_state.cur_section)
-                        st.rerun()
-                        
-
-            # Second Chat Window (Right)
-            with col2:
-                # Selection Window on Top of the Second Chat Box
-                st.markdown("### CPU")
-
-                with st.chat_message("AI"):
-                        st.write("Beep Boop. Prepare yourself.")
-        else:
-            for i in range(len(st.session_state.human_history[1:2+num_args])):
-                hm, cm = st.session_state.human_history[i], st.session_state.cpu_history[i] 
-                col1, middle, col2 = st.columns([1, 2, 1])
-
-                # session state
-                with col1:
-                    st.markdown("### HUMAN")
-                    with st.chat_message("Human"):
-                        st.write(hm.content)
-                                
-                        
-            st.session_state.selected_option = ""
-
-            with middle:
-                options = [
-                    "Appeal to Emotion.",
-                    "Use Facts and Logic.",
-                    "Get Confrontational.",
-                    "Tell a Story."
-                ]
-                for choice in options:
-                    # Assume users will only pick one
-                    if st.button(label=choice, key=choice + "1"):
-                        print(st.session_state.selected_option)
-                        st.session_state.selected_option = choice
-
-                        yap = player.respond(strategy=st.session_state.selected_option, typ="response")
-                        print(yap)
-                        st.session_state.human_history.append(HumanMessage(content=yap))
-                        
-                        response = lawyer.respond(typ="response")
-                        st.session_state.cpu_history.append(AIMessage(content=response))
-                        
-                        if len(st.session_state.human_history) >= 1:
-                            st.session_state.cur_section = 1
-                        elif len(st.session_state.human_history) >= 1 + num_args:
-                            st.session_state.cur_section = 2
-                        elif len(st.session_state.human_history) >= 1 + num_args + 1:
-                            st.session_state.cur_section = 3 # Judgement & Feedback
-                        
-                        print(len(st.session_state.human_history), len(st.session_state.cpu_history), st.session_state.cur_section)
-                        st.rerun()
-                        
-
-            # Second Chat Window (Right)
-            with col2:
-                # Selection Window on Top of the Second Chat Box
-                st.markdown("### CPU")               
-                with st.chat_message("AI"):
-                    st.write(cm.content)
-
+                
+                
 
 # for message in st.session_state.chat_history[:2]: # All openers
 #     if isinstance(message, AIMessage):
--- lawyer-off/player_filler.py
+++ lawyer-off/player_filler.py
@@ -16,81 +16,28 @@ class PlayerGenerator:
         self.model = init_chat_model("gpt-4o-mini", model_provider="openai")
         self.messages = [
             SystemMessage(content="""You are the most capable prosecutor in history. You are never wrong. You always win court cases using strong, persuasive arguments 
-                          backed by facts, logic, and evidence. You also dismantle your opponent's arguments using facts and logic.
-                          You are currently serving the court as a lawyer, and will be conversing with another lawyer, who is your opponent.""")
+                          backed by facts, logic, and evidence.""")
         ]
         self.evidence = evidence
-        
-    def study_case(self, case:str, evidence:List[List]):
-        """Help the lawyer study the case.
-
-        Args:
-            case (str): The main details of the case being discussed.
-            evidence (List[str]): List of evidence found at the scene. 
-                                    Should be of form (type,description)
-        """
-        # change behavior based on if they are attacking or defending
-        self.messages += [
-            SystemMessage(content="This is the case being discussed:\n" + case + "\n"),
+    
+                
+    def respond(self, case, query):
+        messages = self.messages + [
+            SystemMessage(content=f"Use this case to answer the following user query:\n{case}\n"),
+            HumanMessage(content=query)
         ]
-        self.evidence = evidence
-                
-    def respond(self, strategy, typ="response", response:str=None):
-        if response: self.messages.append(HumanMessage(content=response))
         
-        match typ:
-            case "opener":
-                prompt = self.messages + [
-                    SystemMessage(f"Generate an opening statement from the prosecution's perspective using this strategy:{strategy}. Limit output to 60 words or less.")
-                ]
-                response = self.model.invoke(prompt).content
-                return response
-                
-            case "response":
-                response = self.model.invoke(self.messages).content
-                
-                # 2. Check all pieces of evidence if any support the argument
-                for type, description in self.evidence:
-                    tmp = [
-                        SystemMessage(content=f"""Can the following piece of evidence be used to fortify this response? 
-                                            response:{response}
-                                            evidence:{type} evidence, {description}  
-                                            
-                                            Start your response with "YES" or "NO".
-                                    """)
-                    ]
-                    can_be_used = True
-                    tmp_res = self.model.invoke(tmp).content
-                    for denial in ['no', 'No', 'NO']:
-                        if denial in tmp_res:
-                            can_be_used = False
-                            break
-                    
-                    if can_be_used:
-                        incorporate_template = [
-                            SystemMessage(f"""Incorporate this evidence into the argument. 
-                                                evidence: {type} evidence, {description}
-                                                argument: {response}
-                                                
-                                                Include ONLY the update argument in your response.
-                                          """)
-                        ]
-                        # 3. Incorporate evidence into argument.
-                        response = self.model.invoke(incorporate_template).content
-                
-                summarized = self.model.invoke([SystemMessage(content="Summarize the following to 60 words or less:" + response)]).content
+        response = self.model.invoke(messages).content                
+        summarized = self.model.invoke([SystemMessage(content="Summarize the following to 60 words or less:" + response)]).content
                        
-                # 4. Verify it is from a lawyer perspective
-                verify_perspective_template = [
-                    SystemMessage(content="Change this response so that it is from the viewpoint of a lawyer who is currently in court."),
-                    HumanMessage(content=summarized)
-                ]
-                response = self.model.invoke(verify_perspective_template).content
-                       
-                return response 
+        # 4. Verify it is from a lawyer perspective
+        verify_perspective_template = [
+            SystemMessage(content="Change this response so that it is from the viewpoint of a lawyer who is currently in court."),
+            HumanMessage(content=summarized)
+        ]
+        response = self.model.invoke(verify_perspective_template).content
                 
-            case "closer":
-                pass
+        return response
             
     def get_arg_history(self):
         return self.messages[1:] # skip instruction message
