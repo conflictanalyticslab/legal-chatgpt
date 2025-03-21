d0d05fe6dd811bba5568c374e3071a4a4dc71a8a
Khoa Nguyen <dangkhoa81122@gmail.com>
2025-03-21 09:39:37 -0400

    feat: new video

--- src/components/demo-video/demo-video.tsx
+++ src/components/demo-video/demo-video.tsx
@@ -7,7 +7,7 @@ export default function DemoVideo({ className }: DemoVideo) {
     <div className="flex items-center justify-center">
       <video
       controls
-      src="/assets/landing_page/landing-page-video.mp4"
+      src="/assets/landing_page/landing-page-video.mov"
       className={cn("shadow-lg rounded-lg lg:max-w-[min(1000px,45vw)] w-full md:w-[min(100%, 44vw)] mx-auto", className)}
       ></video>
     </div>
