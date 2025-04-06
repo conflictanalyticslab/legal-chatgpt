ee1c3887a4d627d30b2ac7008f540577bcbfe6ba
Khoa Nguyen <dangkhoa81122@gmail.com>
2025-04-06 16:39:50 -0400

    fix: reworked

---
+++ src/app/api/(routes)/llm/query_ex/route.ts
@@ -0,0 +1,39 @@
+"use server";
+import { NextRequest, NextResponse } from "next/server";
+import { getFirestore } from "firebase-admin/firestore";
+import { authenticateApiUser } from "@/lib/middleware/authenticate-api-user";
+import { initBackendFirebaseApp } from "@/lib/middleware/init-backend-firebase-app";
+import { compileGraph } from "@/app/features/chat/components/dialog-flows/compiler";
+import { makeIterator, iteratorToStream } from "../iterator";
+
+export async function POST(req: NextRequest, res: NextRequest) {
+
+  const { decodedToken } = await authenticateApiUser();
+  if (!decodedToken) {
+    return NextResponse.json(
+      { error: "decodedToken is missing but there was no earlyResponse" },
+      { status: 500 }
+    );
+  }
+
+  initBackendFirebaseApp();
+
+  let req_json = await req.json();
+
+  const firestore = getFirestore();
+
+  const graphQuery = await firestore
+    .collection("graphs")
+    .where("userUid", "==", decodedToken.user_id)
+    .where("id", "==", req_json.dialogFlowId)
+    .get();
+  const data = graphQuery.docs[0].data();
+  const graphDoc= { nodes:data.nodes, edges:data.edges, uid: graphQuery.docs[0].id };
+
+  req_json.dialogFlow = compileGraph(graphDoc.nodes, graphDoc.edges);
+
+  const iterator = makeIterator(req_json);
+  const stream = iteratorToStream(iterator);
+
+  return new Response(stream);
+}
