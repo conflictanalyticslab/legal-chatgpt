38fb08d318acf0005ee3d0c82e6d3464edafc8af
Oleg Lavrovsky <loleg@users.noreply.github.com>
2025-04-07 09:00:33 +0200

    Firebase configuration from JSON file

--- src/lib/middleware/init-backend-firebase-app.ts
+++ src/lib/middleware/init-backend-firebase-app.ts
@@ -1,9 +1,20 @@
 import admin from "firebase-admin";
 
+// var serviceAccount = require("serviceAccountKey.json");
+
 export function initBackendFirebaseApp() {
 
+  
   // change the following to process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT when deploying to production
   if (admin.apps.length === 0) {
+
+    // load Firebase configuration from a JSON file included above
+    if (typeof serviceAccount !== 'undefined') {
+      return admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+      });
+    }
+
     if (!process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT) {
       throw new Error(
         "Missing FIREBASE_ADMIN_SERVICE_ACCOUNT from environment variables"
