75dda1501d981bc0c4407ffaffd4a013e5f629af
Kevin Wang <kayhne999@gmail.com>
2025-05-26 12:56:08 -0400

    moving + renaming pinecone & openai embedding files, swapping to general vectorstore type

---
+++ src/app/api/(routes)/graphs/vectorize/PineconeStore.ts
@@ -0,0 +1,53 @@
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+import { PineconeStore } from "@langchain/pinecone";
+import { embeddings, dimension } from "./OpenAI";
+
+export const initPineconeStore = async (
+  indexName: string,
+  namespace: string
+) => {
+  // Initialize Pinecone client
+  const pc = new PineconeClient({
+    apiKey: process.env.NEXT_PUBLIC_PINECONE_API_KEY
+      ? process.env.NEXT_PUBLIC_PINECONE_API_KEY
+      : "PINECONE KEY NOT FOUND",
+  });
+
+  const indexes = (await pc.listIndexes()).indexes;
+  if (!indexes || !indexes.map((idx: any) => idx.name).includes(indexName)) {
+    console.log(
+      `Could not find index '${indexName}' in vector database indexes.`
+    );
+    await createVectorizedDFIndex(pc, indexName, dimension);
+    console.log(`Created index ${indexName}.`);
+  }
+
+  // Note: unlike indexes, namepsaces are created automatically in Pinecone
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex: pc.Index(indexName),
+    // Maximum number of batch requests to allow at once. Each batch is 1000 vectors.
+    maxConcurrency: 5,
+    namespace,
+  });
+
+  return vectorStore;
+};
+
+const createVectorizedDFIndex = async (
+  pc: PineconeClient,
+  indexName: string,
+  dimension: number
+) => {
+  console.log(`Creating index ${indexName}...`);
+  await pc.createIndex({
+    name: indexName,
+    dimension: dimension,
+    metric: "cosine", // Default for semantic search
+    spec: {
+      serverless: {
+        cloud: "aws", // or "gcp"
+        region: "us-east-1",
+      },
+    },
+  });
+};
--- src/app/api/(routes)/graphs/vectorize/route.ts
+++ src/app/api/(routes)/graphs/vectorize/route.ts
@@ -1,12 +1,11 @@
 import { Document } from "@langchain/core/documents";
-import { PineconeStore } from "@langchain/pinecone";
+import { VectorStore } from "@langchain/core/vectorstores";
+import { Embeddings } from "@langchain/core/embeddings";
 import { NextResponse } from "next/server";
-import { initPineconeClient } from "@/app/api/(routes)/graphs/vectorize/init-pinecone-client"
+import { initPineconeStore } from "@/app/api/(routes)/graphs/vectorize/PineconeStore";
 import { getAuthenticatedUser } from "@/lib/auth/get-authenticated-user";
-import { GraphFlowNode } from "@/app/features/chat/components/dialog-flows/nodes"
-import { embeddings, dimension } from "@/app/api/(routes)/graphs/vectorize/node-embeddings";
+import { GraphFlowNode } from "@/app/features/chat/components/dialog-flows/nodes";
 import invariant from "tiny-invariant";
-        
 
 export async function POST(req: Request) {
   const decodedToken = await getAuthenticatedUser();
@@ -17,60 +16,57 @@ export async function POST(req: Request) {
       { status: 401 }
     );
   }
-  
+
   const body = await req.json();
-  const dfToVectorize : {[k : string] : GraphFlowNode | null} = body.df;
-  
-  const indexName = "vectorized-dialogflows";  
-  const pc = await initPineconeClient(indexName, dimension);
+  const dfToVectorize: { [k: string]: GraphFlowNode | null } = body.df;
 
-  const namespace = body.name;  // Note: unlike indexes, namepsaces are created automatically
-  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
-    pineconeIndex: pc.Index(indexName),
-    // Maximum number of batch requests to allow at once. Each batch is 1000 vectors.
-    maxConcurrency: 5,
-    namespace, 
-  });
-  invariant(vectorStore, "Error occured when attempting to initialize vector DB connection.")
+  const indexName = "vectorized-dialogflows";
+  const vectorStore = await initPineconeStore(indexName, body.name);
+  invariant(
+    vectorStore,
+    "Error occured when attempting to initialize vector DB connection."
+  );
 
-
-  let questions : {[key : string] : string} = {} // nodeID : question
-  for(var key in dfToVectorize){
-    if((await fetchNode(key, vectorStore)).length > 0) await vectorStore.delete({ filter: {external_id: key} }); // Delete old copy
+  let questions: { [key: string]: string } = {}; // nodeID : question
+  for (var key in dfToVectorize) {
+    if ((await fetchNode(key, vectorStore)).length > 0)
+      await vectorStore.delete({ filter: { external_id: key } }); // Delete old copy
 
     const node = dfToVectorize[key];
-    if(node){
-      if(node.type == "switch"){
-        let mcQuestion = "Select the best response for the following question.\n"
+    if (node) {
+      if (node.type == "switch") {
+        let mcQuestion =
+          "Select the best response for the following question.\n";
 
-        // LLM should return condition ID as response. 
+        // LLM should return condition ID as response.
         // We can then use compiler.ts logic to then find the related edges.
         node.data.conditions.map((e) => {
-          mcQuestion += `${e.id}:"${e.body}" \n`
-        })
-        
-        if(node.data.otherwise) mcQuestion += `Otherwise : ${node.data.otherwise.body}`
+          mcQuestion += `${e.id}:"${e.body}" \n`;
+        });
+
+        if (node.data.otherwise)
+          mcQuestion += `Otherwise : ${node.data.otherwise.body}`;
 
         questions[key] = mcQuestion;
-      } 
-      
+      }
+
       try {
         // Only store node data (conditions, thresholds, etc.)
         // Don't store PDF data content as it is already stored in a different index
         const trimmedNode = {
-          ...(node.type == "pdf" ? {label : node.data.label} : node.data), 
-          type: node.type ? node.type : "undefined"
+          ...(node.type == "pdf" ? { label: node.data.label } : node.data),
+          type: node.type ? node.type : "undefined",
         };
 
         // Upsert using nodeId as value to make fetching easier
-        const documentNode : Document = {
-          id: key, 
+        const documentNode: Document = {
+          id: key,
           pageContent: key,
-          metadata: { 
+          metadata: {
             external_id: node.id,
-            nodeData: JSON.stringify(trimmedNode) 
-          }
-        }
+            nodeData: JSON.stringify(trimmedNode),
+          },
+        };
         await vectorStore.addDocuments([documentNode]);
       } catch (error) {
         const errString = `Failed to vectorize node ${key} : ${error}`;
@@ -80,27 +76,26 @@ export async function POST(req: Request) {
           { status: 500 }
         );
       }
-
-    } 
+    }
   }
 
   const mcQuestionId = "MC Questions";
-  try{
-    await vectorStore.delete({ filter: {external_id: mcQuestionId} });
+  try {
+    await vectorStore.delete({ filter: { external_id: mcQuestionId } });
 
     // Upsert questions as own vector
-    const questionDocument : Document = {
+    const questionDocument: Document = {
       id: mcQuestionId,
       pageContent: mcQuestionId,
-      metadata: { 
+      metadata: {
         external_id: mcQuestionId,
-        mcQuestions: JSON.stringify(questions) 
-      }
-    }
+        mcQuestions: JSON.stringify(questions),
+      },
+    };
 
     await vectorStore.addDocuments([questionDocument]);
-  } catch(err){
-    console.log(`Failed to vectorize GraphRAG MC Questions: ${err}`)
+  } catch (err) {
+    console.log(`Failed to vectorize GraphRAG MC Questions: ${err}`);
   }
 
   // Debug code to test vectorization
@@ -118,10 +113,10 @@ export async function POST(req: Request) {
   });
 }
 
-const fetchNode = async (nodeId: string, vectorStore: PineconeStore) => {
+const fetchNode = async (nodeId: string, vectorStore: VectorStore) => {
   /**
    * A node is always closest to itself in a vector space,
    * so fetch k=1 should always return the desired node
    */
-  return await vectorStore.similaritySearch(nodeId, 1); 
-}
+  return await vectorStore.similaritySearch(nodeId, 1);
+};
